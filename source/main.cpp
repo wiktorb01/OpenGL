23587e69177c197f4739563c2ce3e57754c52c71
FightingMyself <wiktor.bialy@outlook.com>
2025-08-08 21:46:29 +0200

    Animate light position in main rendering loop
    Moved light position calculation into the render loop and updated it to animate along the Y-axis using a sine function. The light model matrix is now set each frame, enabling dynamic light movement.

--- source/main.cpp
+++ source/main.cpp
@@ -172,10 +172,7 @@ int main()
     lightVBO.Unbind();
     lightEBO.Unbind();
     lightShader.Activate();
-    glm::vec3 lightPos = glm::vec3(0.0f, 0.0f, -2.0f);
-    glm::mat4 lightModel = glm::mat4(1.0f);
-    lightModel = glm::translate(lightModel, lightPos);
-    lightShader.setMat4("lightModel", lightModel);
+   
     
     
     glm::vec3 lightColor(1.0f, 1.0f, 1.0f);
@@ -198,7 +195,13 @@ int main()
         // Clear the color and depth buffers
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        
+
+
+        glm::vec3 lightPos = glm::vec3(0.0f, sin(currentFrame/4) * 4.0f, -2.0f);
+        glm::mat4 lightModel = glm::mat4(1.0f);
+        lightModel = glm::translate(lightModel, lightPos);
+
+
         camera.updateMatrix(0.1f, 100.0f);
 
         shader.Activate();
@@ -226,6 +229,7 @@ int main()
         // Light
         lightShader.Activate();
         lightVAO.Bind();
+        lightShader.setMat4("lightModel", lightModel);
         lightShader.setVec3("lightColor", lightColor);
         camera.Matrix(lightShader, "camMatrix");
         glDrawElements(GL_TRIANGLES, sizeof(lightIndices)/sizeof(unsigned int), GL_UNSIGNED_INT, 0);
